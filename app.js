12c89bae2a1e7988efae1876cc2cd02e77badb48
Shota999 <ashottmarkarian@gmail.com>
2022-06-14 06:58:00 +0400

    main

---
+++ app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load" , ()=> {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://amindi.ge/api/weather/${lat},${long}`;
+            fetch(api)
+            .then(data =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data);
+            });
+        });
+    }
+});
